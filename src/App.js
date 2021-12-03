97335a6a71653970295419a9e7c1d23f22a1a9c0
trusteddev1325 <trusteddev1325@gmail.com>
2021-12-03 04:15:11 -0800

    add metaverse

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Header from "./components/header/header"
 import Utility from './components/utility/utility';
 import Roadmap from './components/roadmap/roadmap';
+import Metaverse from './components/metaverse/metaverse';
 function App() {
   return (
     <div className="App">
         <Header/>
         <Utility/>
         <Roadmap/>
+        <Metaverse/>
     </div>
   );
 }
